4639a3ef6ab62532e79d826a167923aa54bc92eb
Isaac Hailperin <isha@lcsys.ch>
2015-07-02 07:51:02 +0200

    asciidoc gets created

--- base_report.py
+++ base_report.py
@@ -3,6 +3,8 @@ A small reporting tool, using asciidoc
 
 """
 
+import calendar
+import datetime
 import jinja2
 import os
 import sys
@@ -26,8 +28,9 @@ class BaseReport(object):
     def __init__(self, 
         body_data=None,
         body_template=None,
-        output_dir='./reports',
-        tmp_dir='./tmp',):
+        output_filename=None,
+        output_dir='reports',
+        tmp_dir='tmp',):
         """
         Create a report object
 
@@ -42,14 +45,29 @@ class BaseReport(object):
         if not body_template:
             raise BaseReportAttributeMissing('body_template')
         self.body_data = body_data
+        if output_filename: 
+            self.output_filename = output_filename
+        else:
+            self.output_filename = 'report_' + self.today()
         self.body_template = body_template
         self.output_dir = output_dir
         self.tmp_dir = tmp_dir
-        # cretae template environment
-        #self.template_loader = jinja2.FileSystemLoader(searchpath='/')
-        self.template_loader = jinja2.FileSystemLoader(searchpath=os.getcwd())
-        self.template_env = jinja2.Environment(loader=self.template_loader)
-        self._render_template(template=body_template, data=body_data)
+        self.asciidoc = self._render_template(template=body_template, data=body_data)
+
+
+    @staticmethod
+    def today():
+        """
+        Return today formatted as dd-mmm-yyyy
+
+        """
+        today = datetime.datetime.today()
+        day = str(today.day)
+        if len(day) == 1: day = '0' + day
+        month = calendar.month_abbr[today.month]
+        year = str(today.year)
+        return day + '-' + month + '-' + year
+
 
     def _render_template(self, template=None, data=None):
         """
@@ -58,13 +76,33 @@ class BaseReport(object):
         template: the template file
         data: the data the template should be rendered with
         """
+        try:
+            self.template_env
+        except AttributeError:
+            #self.template_loader = jinja2.FileSystemLoader(searchpath='/')
+            self.template_loader = jinja2.FileSystemLoader(searchpath=os.getcwd())
+            self.template_env = jinja2.Environment(loader=self.template_loader)
         loaded_template = self.template_env.get_template(template)
         return loaded_template.render(data)
 
+
+    def _write_asciidoc(self):
+        """
+        Write rendered asciidoc to disk for further processing.
+
+        One could probably just keep it in memory, but it is nice
+        for debugging to see the asciidoc source. 
+        """
+        self.asciidoc_filename = os.getcwd() + '/' + self.tmp_dir + '/' + self.output_filename + '.txt'
+        with open(self.asciidoc_filename, 'w') as filehandle:
+            filehandle.write(self.asciidoc)
 
 
     def render_to_pdf(self):
         """
         Convert asciidoc to pdf
+
         """
-        raise Exception('Continue implementation here')
+        self._write_asciidoc()
+
+
--- tests/test_base_report.py
+++ tests/test_base_report.py
@@ -2,6 +2,7 @@ import unittest
 import os
 import sys
 import mock
+import re
 
 path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if not path in sys.path:
@@ -24,7 +25,27 @@ class TestBaseReport(unittest.TestCase):
             'title': 'A simple test report',
             'author': 'Isaac Hailperin',
             'content': 'Lorem ipsum', }
-        body_template = 'tests/templates/simple_report.txt'
-        report = self.BaseReport(body_data=body_data, body_template=body_template)
+        report = self.BaseReport(
+            body_data=body_data, 
+            body_template='tests/templates/simple_report.txt',
+            output_dir='tests/reports',
+            tmp_dir='tests/tmp',)
         report.render_to_pdf()
+        today = self.BaseReport.today()
+        expected_asciidoc_file = 'tests/tmp/report_' + today + '.txt'
+        expected_asciidoc = """= A simple test report
+Isaac Hailperin
+
+
+Lorem ipsum"""
+        with open(expected_asciidoc_file, 'r') as fh:
+            actual_asciidoc = fh.read()
+        self.assertEqual(expected_asciidoc, actual_asciidoc)
+        self.assertEqual('Implement render_to_pdf()', 'Not implemented')
         
+    def test_today(self):
+        """
+        "Today" string constructred correctly
+        """
+        today = self.BaseReport.today()
+        self.assertTrue(re.match('[0-9]{2}-[A-Za-z]{3}-[0-9]{4}', today))
